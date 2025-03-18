559a521e9c8f095ce80b07d01def35a9ba99e6f4
unknown <kristyna.savelova@infis.cz>
2025-03-18 09:35:22 +0100

    text

---
+++ ukol1.py
@@ -0,0 +1,4 @@
+jmeno = input("jak se jmenuješ? ")
+vek = input("jaký je tvůj věk? ")
+
+print(f"ahoj, jmenuji se {jmeno} a je mi {vek} let.")
---
+++ ukol2.py
@@ -0,0 +1,10 @@
+cislo1 = input("zadej desetinné číslo")
+cislo2 = input("zadej desetinné číslo")
+
+soucet = cislo1+cislo2 
+rozdil = cislo1-cislo2
+soucin = cislo1*cislo2
+podil = cislo1/cislo2
+
+print(f"Součet: {soucet:.2f}")
+
