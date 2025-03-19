fea9af5db182a594b06cc46523f673905d2db112
Kristynasavelova <kristyna.savelova@infis.cz>
2025-03-19 13:16:05 +0100

    Update ukol2.py

--- ukol2.py
+++ ukol2.py
@@ -1,10 +1,19 @@
-cislo1 = input("zadej desetinné číslo")
-cislo2 = input("zadej desetinné číslo")
+cislo1 = float(input("zadej desetinné číslo "))
+cislo2 = float(input("zadej desetinné číslo "))
 
 soucet = cislo1+cislo2 
 rozdil = cislo1-cislo2
 soucin = cislo1*cislo2
-podil = cislo1/cislo2
+if cislo2 != 0:
+    podil = cislo1/cislo2
+else:
+    podil = 0
 
 print(f"Součet: {soucet:.2f}")
+print(f"rozdíl: {rozdil:.2f}")
+print(f"součin: {soucin:.2f}")
 
+if podil != 0:
+    print(f"podíl: {podil:.2f}")
+else:
+    print("Podíl nelze vypočítat.")
